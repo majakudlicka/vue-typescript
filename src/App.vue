0475f8a9786a292e713382de7dcd59022650d115
Maja Kudlicka <maja.kudlicka@gmail.com>
2018-12-08 19:08:11 +0100

    feat - finished OrderTs component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
   <div id="app">
-    <Counter :msg="3"/>
+    <OrderTS msg="Simple Order App" :order="exampleOrder" />
   </div>
 </template>
 
+
+
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import Counter from './components/Counter.vue';
+import OrderTS from './components/OrderTS.vue';
+
+interface Order {
+    productName: string;
+    quantity: number;
+}
 
 @Component({
     // Components are declared inside @Component decorator
   components: {
-    Counter,
+    OrderTS,
   },
+    // It is also a place where we could declare Vuex's binding helpers if using vuex
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+    exampleOrder: Order = { productName: 'Poster', quantity: 10};
+}
 </script>
 
 <style>
---
+++ src/components/OrderTS.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h3>{{ msg }}</h3>
+    <div>{{order.productName}} </div>
+    <div> has quantity of {{ order.quantity }} </div>
+    <div> which is {{isAboveThreshold}}</div>
+    <div>
+      <button @click="increment()">INCREASE Order Quantity</button>
+      <br>
+      <button @click="decrement()">DECREASE Order Quantity</button>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+ //  Lang="ts"
+import { Component, Prop, Watch, Vue } from 'vue-property-decorator';
+
+interface Order {
+    productName: string;
+    quantity: number;
+}
+
+@Component
+//  We export class instead of object
+export default class OrderTS extends Vue {
+
+    // Props are declared using @Prop decorator
+  @Prop({ default: 'default value' }) private msg!: string;
+
+  @Prop({type: Object as () => Order}) private order!: Order;
+
+  //  Data is declared as instance properties
+  private bucketThreshold: number = 15;
+
+  // Methods are declared as instance methods
+  public increment(): void { this.order.quantity++; }
+  public decrement(): void { this.order.quantity--; }
+
+  // Lifecycle hooks
+  public created(): void {
+      console.log('Hello from created hook!');
+  }
+
+  // Watchers are replaced with @Watch decorator
+  @Watch('order', { immediate: true, deep: true })
+  onMessageChanged(val: Order) {
+      console.log(`Order quantity is ${val.quantity}`);
+  }
+
+  // Computed props are defined as getters
+  get isAboveThreshold(): string {
+        return (this.order.quantity < this. bucketThreshold) ? 'below bucket threshold' : 'above bucket threshold';
+  }
+}
+</script>
+
+<style scoped>
+
+h3 {
+  margin: 40px 0 0;
+}
+
+</style>
